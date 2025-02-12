646d03f2eeb0ffdf7eee3e5da2adfb035baccc0d
reactWebDeveloper <sh.layal.12.12@gmail.com>
2025-02-12 16:57:07 +0300

    still working

--- src/Components/ProductComponent/ProductComponent.jsx
+++ src/Components/ProductComponent/ProductComponent.jsx
@@ -5,18 +5,18 @@ export default function ProductComponent({img, title, price, fit, category}) {
   return (
     <div>
       <div className="es-product-card d-flex flex-column">
-        <div className="es-product img">
-          <img src={img} alt={title} />
+        <div className="es-product-img">
+          <img src={img} alt={title} className='img-fluid' />
         </div>
         <div className="es-product-card-row d-flex justify-content-between">
           <div className="es-category"><span>{category}</span></div>
-          <button>Shop Now</button>
+          <button type='button' className='es-card-btn'>Shop Now</button>
         </div>
         <div className="es-product-title">{title}</div>
-        <div className="es-product-details d-flex">
-          <div className="es-product-fit"><span>Fit.</span>{fit}</div>
+        <div className="es-product-details d-flex text-18">
+          <div className="es-product-fit "><span>Fit.</span>{fit}</div>
           <div className="es-product-price"><span>Price.</span>${price}</div>
-        </div>
+        </div >
       </div>
     </div>
   )
--- src/Components/ProductsSectionComponent/ProductsSectionComponent.jsx
+++ src/Components/ProductsSectionComponent/ProductsSectionComponent.jsx
@@ -1,25 +1,41 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './ProductsSectionComponent.css'
-import {productsData} from '../../Data/productsData.jsx';
+import { productsData } from '../../Data/productsData.jsx';
 import ProductComponent from '../ProductComponent/ProductComponent.jsx';
 export default function ProductsSectionComponent() {
+  const [activeTab, setActiveTab] = useState('Women');
+  const handleTabClick = (category) => {
+    setActiveTab(category);
+  }
+  const filteredProducts = 
+        activeTab == 'All' ? [...productsData.womenProductsData, ...productsData.kidsProductsData, ...productsData.menProductsData]
+        : activeTab == "Women" ? productsData.womenProductsData
+        : activeTab == "Men" ? productsData.menProductsData
+        : productsData.kidsProductsData;
   return (
     <div className='p-162 es-main-products-container'>
       <section className='es-products-section border1 d-flex flex-column'>
         <div className="es-upper d-flex flex-column">
           <div className="es-title"></div>
           <ul className="es-tabs d-flex">
-            <li className="es-tab border1 text-18">All</li>
-            <li className="es-tab border1 text-18">Men</li>
-            <li className="es-tab es-active-tab border1 text-18">Women</li>
-            <li className="es-tab border1 text-18">Kids</li>
+            {['All', 'Men', 'Women', 'Kids'].map((category) => (
+              <li className={`es-tab border1 text-18 ${activeTab == category ? 'es-active-tab' : ""}`} 
+                  onClick={() => handleTabClick(category)}>
+                  {category}
+              </li>
+            ))}
           </ul>
         </div>
-        <div className="es-products">
-        {productsData.map((product) => {
-          <ProductComponent img={product.img} title={product.title} category={product.category} fit={product.fit} price={product.price}/>
-        })}
-     
+        <div className="es-products d-flex flex-wrap">
+          {filteredProducts.map((product) => (
+            <ProductComponent
+              img={product.img}
+              title={product.title}
+              category={product.category}
+              fit={product.fit}
+              price={product.price}
+            />
+          ))}
         </div>
       </section>
     </div>
