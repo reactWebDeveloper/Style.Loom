321564709b9e43779370eab91a55b18cce85e9c8
hasanWhatfa <wtfyh05@gmail.com>
2025-02-15 01:47:33 +0300

    Title Component & Steps Component

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,11 +1,10 @@
 import React from 'react'
+import StepsComponent from '../../components/StepsComponent/StepsComponent'
 
 export default function Home() {
   return (
     <div>
-      <h2 className="p-162">
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Numquam, vel nulla delectus, quisquam similique cupiditate deserunt et vitae dolore molestiae harum autem labore nesciunt deleniti dolorum consequatur facilis nemo quidem.
-      </h2>
+      <StepsComponent />
     </div>
   )
 }
