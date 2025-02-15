c7a3f5aaebc0fed9e835a05300a1f250830d8925
mustafa93rashid <mustafa.rashid.abbas@gmail.com>
2025-02-15 19:34:13 +0300

    Navbar first task

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,25 @@
-import { Route, Routes } from 'react-router-dom'
-import './App.css'
-import Home from './Pages/Home/Home'
-import Products from './Pages/Products/Products'
-import ProductDetails from './Pages/ProductDetails/ProductDetails'
-import Contact from './Pages/Contact/Contact'
-import Cart from './Pages/Cart/Cart'
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
+import Home from "./Pages/Home/Home";
+import Products from "./Pages/Products/Products";
+import ProductDetails from "./Pages/ProductDetails/ProductDetails";
+import Contact from "./Pages/Contact/Contact";
+import Cart from "./Pages/Cart/Cart";
+import Navbar from "./Components/Navbar/Navbar";
 
 function App() {
-
   return (
-    <div className='es-bg-black'>
-    <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/products" element={<Products />} />
-          <Route path="/productDetails" element={<ProductDetails />} />
-          <Route path="/contact" element={<Contact />} />
-          <Route path="/cart" element={<Cart />} />
+    <div className="es-bg-black">
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/products" element={<Products />} />
+        <Route path="/productDetails" element={<ProductDetails />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/cart" element={<Cart />} />
       </Routes>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,10 +1,8 @@
 import React from 'react'
-import Navbar from '../../Component/Navbar/Navbar'
 
 export default function Home() {
   return (
     <div>
-      <Navbar />
       
     </div>
   )
