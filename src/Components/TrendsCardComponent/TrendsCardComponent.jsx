07279ec1c0c5cdee95fc11dbdb264c4c0ec89e49
alaa-assas <alaa.assas1997@gmail.com>
2025-02-17 07:47:25 +0300

    responsive trends section

--- src/Components/TrendsCardComponent/TrendsCardComponent.jsx
+++ src/Components/TrendsCardComponent/TrendsCardComponent.jsx
@@ -5,9 +5,12 @@ export default function TrendsCardComponent({data}) {
   return (
     <div className="AA-trends-card ">
         <img className="AA-bg" src={data.bg} alt="bg" />
-        <img className="AA-icon" src={data.icon} alt="icon"/>
-        <h4 className="AA-card-title">{data.title}</h4>
-        <p className='AA-card-desc'>{data.desc}</p>
+        <picture>
+          <source media='(max-width:1919px)' srcSet={data.iconsm}/>
+          <img className="AA-icon" src={data.icon} alt="icon"/>
+        </picture>
+        <h4 className="AA-card-title text-24">{data.title}</h4>
+        <p className='AA-card-desc text-18'>{data.desc}</p>
     </div>
   )
 }
--- src/Data/TrendsData.jsx
+++ src/Data/TrendsData.jsx
@@ -1,9 +1,15 @@
 import icon1 from "../assets/imgs/Trends/Icon-Container.png"
+import iconsm1 from "../assets/imgs/Trends/sm-Icon-Container.png"
 import icon2 from "../assets/imgs/Trends/Icon-Container-1.png"
+import iconsm2 from "../assets/imgs/Trends/sm-Icon-Container-1.png"
 import icon3 from "../assets/imgs/Trends/Icon-Container-2.png"
+import iconsm3 from "../assets/imgs/Trends/sm-Icon-Container-2.png"
 import icon4 from "../assets/imgs/Trends/Icon-Container-3.png"
+import iconsm4 from "../assets/imgs/Trends/sm-Icon-Container-3.png"
 import icon5 from "../assets/imgs/Trends/Icon-Container-4.png"
+import iconsm5 from "../assets/imgs/Trends/sm-Icon-Container-4.png"
 import icon6 from "../assets/imgs/Trends/Icon-Container-15.png"
+import iconsm6 from "../assets/imgs/Trends/sm-Icon-Container-5.png"
 import bg1 from "../assets/imgs/Trends/Icon-10.png"
 import bg2 from "../assets/imgs/Trends/Icon-11.png"
 import bg3 from "../assets/imgs/Trends/Icon-12.png"
@@ -19,6 +25,7 @@ const TrendsData = storedValueTrendsData ? JSON.parse(storedValueTrendsData) : [
     desc:
       "Every garment at StyleLoom is crafted with passion, reflecting our commitment to quality and innovation.",
     icon: icon1,
+    iconsm: iconsm1,
     bg: bg1
   },
   {
@@ -26,6 +33,7 @@ const TrendsData = storedValueTrendsData ? JSON.parse(storedValueTrendsData) : [
     desc:
       "We're more than a brand; we're trendsetters, curating styles that empower and inspire confidence.",
     icon: icon2,
+    iconsm: iconsm2,
     bg: bg2
   },
   {
@@ -33,6 +41,7 @@ const TrendsData = storedValueTrendsData ? JSON.parse(storedValueTrendsData) : [
     desc:
       "At StyleLoom, our customers are at the heart of everything we do. Your satisfaction is our measure of success.",
     icon: icon3,
+    iconsm: iconsm3,
     bg: bg3
   },
   {
@@ -40,6 +49,7 @@ const TrendsData = storedValueTrendsData ? JSON.parse(storedValueTrendsData) : [
     desc:
       "Influenced by global trends, we bring you a diverse and dynamic collection, embodying the spirit of fashion from around the world.",
     icon: icon4,
+    iconsm: iconsm4,
     bg: bg4
   },
   {
@@ -47,6 +57,7 @@ const TrendsData = storedValueTrendsData ? JSON.parse(storedValueTrendsData) : [
     desc:
       "Beyond clothing, StyleLoom is a lifestyle. Join us on a journey of self-expression and empowerment through fashion.",
     icon: icon5,
+    iconsm: iconsm5,
     bg: bg5
   },
   {
@@ -54,6 +65,7 @@ const TrendsData = storedValueTrendsData ? JSON.parse(storedValueTrendsData) : [
     desc:
       "StyleLoom is committed to sustainability, integrating eco-friendly practices into our production process.",
     icon: icon6,
+    iconsm: iconsm6,
     bg: bg6
   }
 ];
